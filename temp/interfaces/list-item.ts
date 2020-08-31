1bb2d54add1fa8a41d004b726acccfb54b0044fd
Jeffrey Pluntke <jeffrey.pluntke@student.uni-luebeck.de>
2020-08-31 11:01:18 +0200

    Fix .env.example && Merge with feature/splitFrontendBackend

--- temp/interfaces/list-item.ts
+++
@@ -1,5 +0,0 @@
-export default interface ListItem{
-  name: String;
-  tags: String[];
-  isFavorite: Boolean;
-}
--- temp/js/index.ts
+++
@@ -1,5 +0,0 @@
-// create store
-// const store = createStore();
-// console.log(store);
-// TODO: IMPORT ERROR for store
-export = null;
