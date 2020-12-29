c0f6680f8b48ac3b1f7b45534280ce09c3207065
Jeffrey Pluntke <jeffrey.pluntke@student.uni-luebeck.de>
2020-12-29 10:12:59 +0100

    Fix whitelist in CorsConfig

--- backend/src/config/CorsConfig.ts
+++ backend/src/config/CorsConfig.ts
@@ -1,6 +1,11 @@
 import { Request } from 'express';
 // Array for whitelisted URLs
-const whitelist = ['http://localhost:9090'];
+const whitelist = [
+	'http://localhost:9090',
+	'http://127.0.0.1:9090',
+	'http://localhost:9091',
+	'http://127.0.0.1:9091',
+];
 
 interface CorsOptions {
 	origin: boolean;
@@ -15,6 +20,7 @@ export default function corsConfig(
 	req: Request, callback: (a: null, b: CorsOptions) => void,
 ): void {
 	const origin = req.header('Origin');
+	console.log(origin);
 	const isOriginWhitelisted = whitelist.includes(origin);
 	callback(null, { origin: isOriginWhitelisted });
 }
